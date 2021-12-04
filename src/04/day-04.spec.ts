f921519a3f03e99bf08161f7b2a5295242c6ab55
Ross Bratton <bratton.ross@gmail.com>
2021-12-04 23:24:04 +0000

    day 4 part 2

--- src/04/day-04.spec.ts
+++ src/04/day-04.spec.ts
@@ -1,4 +1,4 @@
-import { part1 } from './day-04';
+import { part1, part2 } from './day-04';
 
 describe('part 1', () => {
   it('should return the final score of the winning board', () => {
@@ -25,5 +25,33 @@ describe('part 1', () => {
     const actual = part1(input);
 
     expect(actual).toBe(4512);
+  });
+});
+
+describe('part 2', () => {
+  it('should return the score of the board that wins last', () => {
+    const input = `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7`;
+
+    const actual = part2(input);
+
+    expect(actual).toBe(1924);
   });
 });
