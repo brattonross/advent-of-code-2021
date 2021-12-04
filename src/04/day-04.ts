67c061834fe234087d0df4bc705f4ef4bb431f82
Ross Bratton <bratton.ross@gmail.com>
2021-12-04 22:58:37 +0000

    day 4 part 1

---
+++ src/04/day-04.ts
@@ -0,0 +1,55 @@
+const part1 = (input: string) => {
+  const lines = input.split('\n');
+
+  // Build an array of 2d arrays that represent each board.
+  let boards = [];
+  let board = [];
+  for (let i = 2; i < lines.length; i++) {
+    const line = lines[i];
+
+    if (!line) {
+      boards.push(board);
+      board = [];
+      continue;
+    }
+
+    board.push(line.split(' ').filter(Boolean).map(Number));
+    if (i === lines.length - 1) {
+      boards.push(board);
+    }
+  }
+
+  // Iterate over the sequence of numbers that are called,
+  // and determine after each one whether there is a winner.
+  const nums = lines[0].split(',').map(Number);
+  for (let num of nums) {
+    for (let board of boards) {
+      for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+          if (board[i][j] === num) {
+            // Replace the number with -1 to indicate that it has been called.
+            board[i][j] = -1;
+
+            // Check if this board has won.
+            if (
+              board[i].every((n) => n === -1) ||
+              board.every((arr) => arr[j] === -1)
+            ) {
+              let sum = 0;
+              for (let k = 0; k < board.length; k++) {
+                sum += board[k]
+                  .filter((n) => n > -1)
+                  .reduce((acc, cur) => acc + cur, 0);
+              }
+              return sum * num;
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+const part2 = (input: string) => {};
+
+export { part1 };
---
+++ src/04/index.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+import { part1 } from './day-04';
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), {
+  encoding: 'utf-8',
+});
+
+const solution1 = part1(input);
+console.log('Part 1: ', solution1);
+
+// const solution2 = part2(input);
+// console.log('Part 2: ', solution2);
