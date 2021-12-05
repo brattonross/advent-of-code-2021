dcf9914c1a7b3a3a71e27a595549025f6d3deb45
Ross Bratton <bratton.ross@gmail.com>
2021-12-05 20:49:55 +0000

    day 5 part 2

--- src/05/day-05.spec.ts
+++ src/05/day-05.spec.ts
@@ -1,4 +1,4 @@
-import { part1 } from './day-05';
+import { part1, part2 } from './day-05';
 
 describe('part 1', () => {
   it('should return the number of points at which at least 2 lines overlap', () => {
@@ -16,5 +16,24 @@ describe('part 1', () => {
     const actual = part1(input);
 
     expect(actual).toBe(5);
+  });
+});
+
+describe('part 2', () => {
+  it('should return the number of points at which at least 2 lines overlap', () => {
+    const input = `0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2`;
+
+    const actual = part2(input);
+
+    expect(actual).toBe(12);
   });
 });
--- src/05/day-05.ts
+++ src/05/day-05.ts
@@ -1,44 +1,65 @@
-const part1 = (input: string) => {
-  const parsed = input
+const parseLines = (arr: string) =>
+  arr
     .split('\n')
     .filter(Boolean)
     .map((s) =>
       s.split(' -> ').map((coord) => coord.trim().split(',').map(Number))
     );
 
+const countLine = (line: Array<Array<number>>, map: Map<string, number>) => {
+  const [start, end] = line;
+
+  const incrementCount = (x: number, y: number) => {
+    const key = `${x},${y}`;
+    map.set(key, (map.get(key) ?? 0) + 1);
+  };
+
+  if (start[0] === end[0]) {
+    if (start[1] > end[1]) {
+      for (let y = start[1]; y >= end[1]; y--) {
+        incrementCount(start[0], y);
+      }
+    } else {
+      for (let y = start[1]; y <= end[1]; y++) {
+        incrementCount(start[0], y);
+      }
+    }
+  } else if (start[1] === end[1]) {
+    if (start[0] > end[0]) {
+      for (let x = start[0]; x >= end[0]; x--) {
+        incrementCount(x, start[1]);
+      }
+    } else {
+      for (let x = start[0]; x <= end[0]; x++) {
+        incrementCount(x, start[1]);
+      }
+    }
+  } else {
+    let xStep = start[0] > end[0] ? -1 : 1;
+    let yStep = start[1] > end[1] ? -1 : 1;
+
+    let y = start[1];
+    for (
+      let x = start[0];
+      xStep === -1 ? x >= end[0] : x <= end[0];
+      x += xStep
+    ) {
+      incrementCount(x, y);
+      y += yStep;
+    }
+  }
+};
+
+const part1 = (input: string) => {
+  const parsed = parseLines(input);
   const validLines = parsed.filter(
     ([start, end]) => start[0] === end[0] || start[1] === end[1]
   );
 
   const counts = new Map<string, number>();
 
-  const incrementCount = (x: number, y: number) => {
-    const key = `${x},${y}`;
-    counts.set(key, (counts.get(key) ?? 0) + 1);
-  };
-
-  for (let [start, end] of validLines) {
-    if (start[0] === end[0]) {
-      if (start[1] > end[1]) {
-        for (let y = start[1]; y >= end[1]; y--) {
-          incrementCount(start[0], y);
-        }
-      } else {
-        for (let y = start[1]; y <= end[1]; y++) {
-          incrementCount(start[0], y);
-        }
-      }
-    } else {
-      if (start[0] > end[0]) {
-        for (let x = start[0]; x >= end[0]; x--) {
-          incrementCount(x, start[1]);
-        }
-      } else {
-        for (let x = start[0]; x <= end[0]; x++) {
-          incrementCount(x, start[1]);
-        }
-      }
-    }
+  for (let line of validLines) {
+    countLine(line, counts);
   }
 
   let total = 0;
@@ -50,4 +71,22 @@ const part1 = (input: string) => {
   return total;
 };
 
-export { part1 };
+const part2 = (input: string) => {
+  const validLines = parseLines(input);
+
+  const counts = new Map<string, number>();
+
+  for (let line of validLines) {
+    countLine(line, counts);
+  }
+
+  let total = 0;
+  for (let val of counts.values()) {
+    if (val > 1) {
+      total++;
+    }
+  }
+  return total;
+};
+
+export { part1, part2 };
