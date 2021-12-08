fa1563d6f4e3ef80691cad5fdfb16ae3badbc8b1
Ross Bratton <bratton.ross@gmail.com>
2021-12-08 23:57:44 +0000

    day 6 part 2

--- src/06/day-06.spec.ts
+++ src/06/day-06.spec.ts
@@ -1,4 +1,4 @@
-import { part1 } from './day-06';
+import { part1, part2 } from './day-06';
 
 describe('part 1', () => {
   it('should return the number of lanternfish after 80 days', () => {
@@ -7,5 +7,15 @@ describe('part 1', () => {
     const actual = part1(input);
 
     expect(actual).toBe(5934);
+  });
+});
+
+describe('part 2', () => {
+  it('should return the number of lanternfish after 256 days', () => {
+    const input = '3,4,3,1,2';
+
+    const actual = part2(input);
+
+    expect(actual).toBe(26984457539);
   });
 });
--- src/06/day-06.ts
+++ src/06/day-06.ts
@@ -21,4 +21,29 @@ const part1 = (input: string) => {
   return nums.length;
 };
 
-export { part1 };
+const part2 = (input: string) => {
+  const DAYS = 256;
+  // Store the total number of fish for the numbers 0-8 that have an internal counter of that number.
+  const internalCounters = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const fish = input.split(',').map(Number);
+
+  // Set up the inital state.
+  for (let internalCounter of fish) {
+    internalCounters[internalCounter]++;
+  }
+
+  for (let i = 0; i < DAYS; i++) {
+    // Shift the first number of fish (internal counter 0) off of the array.
+    // This moves all of the other counts into the correct new position as well.
+    const fishProduced = internalCounters.shift()!;
+
+    // Now we need to add a new fish for each one that had an internal count of 0,
+    // as well as set each of those existing fish to have a timer of 6.
+    internalCounters.push(fishProduced);
+    internalCounters[6] += fishProduced;
+  }
+
+  return internalCounters.reduce((acc, cur) => acc + cur, 0);
+};
+
+export { part1, part2 };
