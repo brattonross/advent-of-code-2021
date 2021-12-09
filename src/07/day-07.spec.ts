e68ac249351f78ea7d332e95704ad6706fe4ca5d
Ross Bratton <bratton.ross@gmail.com>
2021-12-09 01:05:15 +0000

    day 7 part 2

--- src/07/day-07.spec.ts
+++ src/07/day-07.spec.ts
@@ -1,4 +1,4 @@
-import { part1 } from './day-07';
+import { part1, part2 } from './day-07';
 
 describe('part 1', () => {
   it('should return minimum fuel amount to align the horizontal positions', () => {
@@ -7,5 +7,15 @@ describe('part 1', () => {
     const actual = part1(input);
 
     expect(actual).toBe(37);
+  });
+});
+
+describe('part 2', () => {
+  it('should return minimum fuel amount to align the horizontal positions', () => {
+    const input = '16,1,2,0,4,2,7,1,2,14';
+
+    const actual = part2(input);
+
+    expect(actual).toBe(168);
   });
 });
--- src/07/day-07.ts
+++ src/07/day-07.ts
@@ -7,4 +7,37 @@ const part1 = (input: string) => {
   return nums.reduce((acc, cur) => acc + Math.abs(cur - median), 0);
 };
 
-export { part1 };
+const part2 = (input: string) => {
+  const nums = input.split(',').map(Number);
+
+  // Figure out the range of positions that are viable
+  let min = Infinity;
+  let max = 0;
+  for (let num of nums) {
+    if (num > max) {
+      max = num;
+    } else if (num < min) {
+      min = num;
+    }
+  }
+
+  // Determine minimum fuel required.
+  let minFuel = Infinity;
+  for (let pos = min; pos < max; pos++) {
+    let fuelUsed = 0;
+    for (let num of nums) {
+      const distance = Math.abs(num - pos);
+      for (let i = 1; i <= distance; i++) {
+        fuelUsed += i;
+      }
+    }
+
+    if (fuelUsed < minFuel) {
+      minFuel = fuelUsed;
+    }
+  }
+
+  return minFuel;
+};
+
+export { part1, part2 };
